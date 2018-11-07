d428c7e2bfc497bca61c3d2e514c24e592c63931
Amanda R Hart <ahart2@umassd.edu>
2018-11-06 18:06:02 -0800

    This is a second draft of my code, the BevertonHolt works ok all around but still has some scaling issues with SSB. The other 2 models need to be checked and currently this code won't compute Fcrash for these models.

--- Hart_HW3/Hart_HW3.R
+++ Hart_HW3/Hart_HW3.R
@@ -20,7 +20,8 @@ HW3Function <- function(FishingMortality = seq(0,1, by=0.01),
                         Rzero = NULL,
                         Steepness = NULL,
                         StockRecruitForm = "BevertonHolt",
-                        gamma = NULL){
+                        gamma_par = NULL,
+                        DoPlot = TRUE){
   # Args
     # FishingMortality: Vector of fishing mortalities to assess
     # M_par: Number between 0 and 1, represents natural mortality, no default
@@ -33,15 +34,197 @@ HW3Function <- function(FishingMortality = seq(0,1, by=0.01),
       # = "BevertonHolt"
       # = "Ricker"
       # = "PellaTomlinson
-    # gamma: number, no default
+    # gamma_par: number, no default
+    # DoPlot: Boolean, specifies if plots are produced, default = TRUE
   
   # Return:
   
+  Ages <- seq(0,length(FecundityAtAge)-1)
   
+
   
+  #################################################################################################################
+  # Storage vector
+  SBPR <- rep(NA, length(FishingMortality))
+  Recruit <- rep(NA, length(FishingMortality))
+  Yield <- rep(NA, length(FishingMortality))
+  SSB <- rep(NA, length(FishingMortality))
+  
+  # Loop over fishing efforts
+  for(ifish in 1:length(FishingMortality)){
+    # # Calculate total mortality at age for both sexes
+    # Z_mortality <- CalcZMortality(FishingMortality = FishingMortality[ifish], M_par = M_par, SelectivityAtAge = SelectivityAtAgeMatrix)
+    # # Calculate abundance at age for both sexes
+    # NAA <- CalcNAA(Z_mortality = Z_mortality, Ages = Ages)
+    # # Calculate Yield Per Recruit
+    # YPR[ifish] <- CalcYPR(FishingMortality = FishingMortality[ifish], WeightAtAge = WeightAtAgeMatrix, SelectivityAtAge = SelectivityAtAgeMatrix, NAA = NAA, Z_mortality = Z_mortality, Ages = Ages)
+    # # Calculate Spawner Biomas Per Recruit
+    # SBPR[ifish] <- CalcSBPR(NAA = NAA, FecundityAtAge = FecundityAtAge, Ages = Ages)
+    # # Calculate recruitment
+    # Recruit[ifish] <- CalcRecruit(FishingMortality = FishingMortality[ifish], StockRecruitForm = "BevertonHolt", SBPR = SBPR[ifish], SBPRzero = SBPRzero, Steepness = Steepness, Rzero = Rzero)
+    
+    # Calculate yield & store corresponding SBPR & Recruitment
+    Result <- CalcYield(FishingMortality = FishingMortality[ifish], M_par = M_par, SelectivityAtAge = SelectivityAtAgeMatrix, WeightAtAge = WeightAtAgeMatrix, FecundityAtAge = FecundityAtAge, 
+                        gamma_par = gamma_par, Ages = Ages, Steepness = Steepness, Rzero = Rzero, StockRecruitForm = StockRecruitForm)
+    SBPR[ifish] <- Result$SBPR
+    #print(Result$Z_mortality)
+    Recruit[ifish] <- Result$Recruit
+    # print(SBPR)
+    # print(SBPRzero)
+    # print(Rzero)
+    # print(Recruit)
+    Yield[ifish] <- Result$Yield
+    NAA <- Result$NAA
+    #print(NAA)
+    #print(Result$YPR)
+    # Calculate Spawning Stock Biomass
+    print("here")
+    print(Result$SBPR)
+    print(Result$Recruit)
+    SSB[ifish] <- CalcSSB(SBPR = Result$SBPR, Recruit = Result$Recruit, FishingMortality = FishingMortality[ifish])
+    print(SSB)
+  }
+  
+  # Calculate MSY and FMSY
+  FMSY <- uniroot(CalcFMSY,interval=c(0,1), StockRecruitForm = StockRecruitForm, gamma_par = gamma_par, Steepness = Steepness, Rzero = Rzero) #, Steepness = Steepness)
+  MSYResults <- CalcYield(FishingMortality = FMSY$root, M_par = M_par, SelectivityAtAge = SelectivityAtAgeMatrix, WeightAtAge = WeightAtAgeMatrix, FecundityAtAge = FecundityAtAge,
+                          gamma_par = gamma_par, Ages = Ages, Steepness = Steepness, Rzero = Rzero, StockRecruitForm = StockRecruitForm)
+  MSY <- MSYResults$Yield
+  print(MSY)
+  print(max(Result$Yield))
+
+  # Calculate Fcrash
+  # Fcrash <- uniroot(CalcFcrash, interval=c(0,1), StockRecruitForm = StockRecruitForm, Steepness = Steepness, Rzero = Rzero)
+  # Fcrash <- Fcrash$root
+  print("FMSY")
+  print(FMSY$root)
+  print("MSY")
+  print(MSY)
+  # print("Fcrash")
+  # print(Fcrash)
+
+  
+  if(DoPlot == TRUE){
+    par(mfrow=c(2,2)) # this makes a 4X4 plot
+    
+    # Subset data
+    plotRecruit <- Recruit[which(Recruit >=0)]
+    plotSSB <- SSB[which(Recruit >= 0)]
+    plotYield <- Yield[which(Recruit >= 0)]
+    plotFishingMortality <- FishingMortality[which(Recruit >= 0)]
+    
+    # Plot
+    plot(x = plotSSB, y = plotRecruit, main = "Recruitment vs. SSB", xlab = "SSB", ylab = "Recruitment")
+    xreplacement <- seq(0:max(SSB))
+    yreplacement <- seq(0:max(SSB))
+    lines(x=xreplacement, y=yreplacement)
+    plot(x = plotSSB, y = plotYield, main = "Yield vs. SSB", xlab = "SSB", ylab = "Yield")
+    abline(h = MSY, col = "red")
+    plot(x = plotFishingMortality, y = plotYield, main = "Yield vs. Fishing Mortality", xlab = "Fishing Mortality", ylab = "Yield")
+    abline(v = FMSY$root, col="red")
+    # abline(v = Fcrash, col = "green")
+    abline(h = MSY, col = "blue")
+    legend("topright", legend=c("FMSY", "MSY", "Fcrash"), fill=c("red", "blue", "green"))
+    plot(x = plotFishingMortality, y = plotSSB, main = "SSB vs. Fishing Mortality", xlab = "Fishing Mortality", ylab = "SSB")
+    mtext(StockRecruitForm, outer = TRUE, cex = 1.5, side = 3, line = -1.5)
+  }
 }
 
-Ages <- seq(0,length(FecundityAtAge)-1)
+# Test with single value of F
+HW3Function(FishingMortality = 0.1, M_par = 0.15, FecundityAtAge = data$Fecundity, WeightAtAgeMatrix = WeightAtAgeMatrix,
+            SelectivityAtAgeMatrix = SelectivityAtAgeMatrix, Rzero = 1, Steepness = 0.5, StockRecruitForm = "BevertonHolt", 
+            gamma_par = 0.2, DoPlot = FALSE)
+
+# Run across a range of F
+HW3Function(FishingMortality = seq(0,1,by=0.001), M_par = 0.2, FecundityAtAge = data$Fecundity, 
+            WeightAtAgeMatrix = WeightAtAgeMatrix, SelectivityAtAgeMatrix = SelectivityAtAgeMatrix, 
+            Rzero = 1, Steepness = 0.5, StockRecruitForm = "Ricker", gamma_par = 0.2, 
+            DoPlot = TRUE)
+
+
+CalcFMSY <- function(F_par, StockRecruitForm = "BevertonHolt", gamma_par = 1, Steepness = NULL, Rzero = NULL){
+  # Args:
+  # F_par is fishing mortality to vary using uniroot
+  # StockRecruitForm is the stock recruit relationship
+  # gamma_par is parameter needed if StockRecruitForm = "PellaTomlinson", default = 1
+  # Steepness of stock recruit relationship
+  # Rzero = recruitment at 0 fishing
+  
+  Result1 <- CalcYield(FishingMortality = (F_par + 0.001), M_par = 0.15, SelectivityAtAge = SelectivityAtAgeMatrix, WeightAtAge = WeightAtAgeMatrix, FecundityAtAge = data$Fecundity, 
+                       gamma_par = gamma_par, Ages = Ages, Steepness = Steepness, Rzero = Rzero, StockRecruitForm = StockRecruitForm)
+  
+  Result2 <- CalcYield(FishingMortality = (F_par - 0.001), M_par = 0.15, SelectivityAtAge = SelectivityAtAgeMatrix, WeightAtAge = WeightAtAgeMatrix, FecundityAtAge = data$Fecundity, 
+                       gamma_par = gamma_par, Ages = Ages, Steepness = Steepness, Rzero = Rzero, StockRecruitForm = StockRecruitForm)
+  print(Result1$Yield)
+  print(Result2$Yield)
+  
+  Difference <- (Result2$Yield - Result1$Yield)/0.002
+  return(Difference)
+}
+
+uniroot(CalcFMSY,interval=c(0,1), StockRecruitForm = "PellaTomlinson", gamma_par = 1, Steepness = 0.5, Rzero=1)
+
+#!!!!!!!!!!!! this doesn't work for Ricker or BevertonHolt
+CalcFcrash <- function(F_par, StockRecruitForm = "BevertonHolt", Steepness = NULL, Rzero = NULL, gamma_par = 1){
+  # Args:
+  # F_par is fishing mortality to vary using uniroot
+  # StockRecruitForm is the stock recruit relationship
+  # Steepness is slope of the stock-recruit relationship
+  # Rzero is recruitment under no fishing
+  # gamma_par: optional parameter passed when 
+  
+  if(StockRecruitForm == "BevertonHolt"){ # This works
+    Result <- CalcYield(FishingMortality = (F_par), M_par = 0.15, SelectivityAtAge = SelectivityAtAgeMatrix, WeightAtAge = WeightAtAgeMatrix, FecundityAtAge = data$Fecundity, 
+                        gamma_par = gamma_par, Ages = Ages, Steepness = Steepness, Rzero = Rzero, StockRecruitForm = "BevertonHolt")
+    alpha <- Result$SBPRzero*(0.2-0.2*Steepness)/(0.8*Steepness)
+    
+    print(Result$SBPR)
+    print(alpha)
+    
+    Difference <- Result$SBPR - alpha
+  } else if (StockRecruitForm == "Ricker"){ # this is set up such that calculating at F=1 and F=0 are both negative differences so there is no zero between them, rethink this
+    Result <- CalcYield(FishingMortality = (F_par), M_par = 0.15, SelectivityAtAge = SelectivityAtAgeMatrix, WeightAtAge = WeightAtAgeMatrix, FecundityAtAge = data$Fecundity, 
+                        gamma_par = gamma_par, Ages = Ages, Steepness = Steepness, Rzero = Rzero, StockRecruitForm = "Ricker")
+    alpha <- Rzero/(Result$SBPRzero*exp(-(log(Steepness/0.2)/0.8)))
+    
+    print(Result$SBPR)
+    print(alpha)
+    
+    Difference <- Result$SBPR - alpha
+  } else if (StockRecruitForm == "PellaTomlinson"){
+    Result <- CalcYield(FishingMortality = (F_par), M_par = 0.15, SelectivityAtAge = SelectivityAtAgeMatrix, WeightAtAge = WeightAtAgeMatrix, FecundityAtAge = data$Fecundity, 
+                        gamma_par = gamma_par, Ages = Ages, Steepness = Steepness, Rzero = Rzero, StockRecruitForm = "Ricker")
+    alpha <- 1/Result$SBPRzero
+    
+    print(Result$SBPR)
+    print(alpha)
+    
+    Difference <- Result$SBPR - alpha
+  }
+  
+  return(Difference)
+}
+
+uniroot(CalcFcrash, interval=c(0,1), StockRecruitForm = "BevertonHolt", Steepness = 0.5, Rzero = 1, gamma_par = 1)
+
+
+
+
+
+
+
+
+
+
+
+
+# max yield 0.02662691 as calculated
+
+
+# Test CalcYield Equation
+ResultTest <- CalcYield(FishingMortality = 0.5, M_par = M_par, SelectivityAtAge = SelectivityAtAgeMatrix, WeightAtAge = WeightAtAgeMatrix, FecundityAtAge = FecundityAtAge, 
+                        Ages = Ages, Steepness = Steepness, Rzero = Rzero, StockRecruitForm = "BevertonHolt")
+
 
 SelectivityAtAgeMatrix <- cbind(data$Selectivity_F, data$Selectivity_M)
 colnames(SelectivityAtAgeMatrix) <- c("Selectivity_F", "Selectivity_M")
@@ -51,42 +234,27 @@ colnames(WeightAtAgeMatrix) <- c("Weight_F", "Weight_M")
 
 
 # Test
-M_par <- 0.2
-Steepness  <- 0.3
-Rzero <- 1000
+M_par <- 0.15
+Steepness  <- 0.5
+Rzero <- 1
 
 
 
-# First calculate SBPRzero
-Z_mortality <- CalcZMortality(FishingMortality = 0, M_par = M_par, SelectivityAtAge = SelectivityAtAgeMatrix)
-NAA <- CalcNAA(Z_mortality = Z_mortality, Ages = Ages)
-SBPRzero <- CalcSBPR(NAA = NAA, FecundityAtAge = data$Fecundity, Ages = Ages)
 
-#################################################################################################################
-# Storage vector
-YPR <- rep(NA, length(FishingMortality))
-SBPR <- rep(NA, length(FishingMortality))
-Recruit <- rep(NA, length(FishingMortality))
-Yield <- rep(NA, length(FishingMortality))
-SSB <- rep(NA, length(FishingMortality))
 
-# Loop over fishing efforts
-for(ifish in 1:length(FishingMortality)){
-  # Calculate total mortality at age for both sexes
-  Z_mortality <- CalcZMortality(FishingMortality = FishingMortality[ifish], M_par = M_par, SelectivityAtAge = SelectivityAtAgeMatrix)
-  # Calculate abundance at age for both sexes
-  NAA <- CalcNAA(Z_mortality = Z_mortality, Ages = Ages)
-  # Calculate Yield Per Recruit
-  YPR[ifish] <- CalcYPR(FishingMortality = FishingMortality[ifish], WeightAtAge = WeightAtAgeMatrix, SelectivityAtAge = SelectivityAtAgeMatrix, NAA = NAA, Z_mortality = Z_mortality, Ages = Ages)
-  # Calculate Spawner Biomas Per Recruit
-  SBPR[ifish] <- CalcSBPR(NAA = NAA, FecundityAtAge = FecunditiyAtAge, Ages = Ages)
-  # Calculate recruitment
-  Recruit[ifish] <- CalcRecruit(FishingMortality = FishingMortality[ifish], StockRecruitForm = "BevertonHolt", SBPR = SBPR[ifish], SBPRzero = SBPRzero, Steepness = Steepness, Rzero = Rzero)
-  # Calculate yield
-  Yield[ifish] <- CalcYield(YPR = YPR[ifish], Recruit = Recruit[ifish])
-  # Calculate Spawning Stock Biomass
-  SSB[ifish] <- CalcSSB(SBPR = SBPR[ifish], Recruit = Recruit[ifish])
-}
+
+# CalcMSY <- function(i){
+#   # Args: FishingMortality = FishingMortality, Yield = Yield
+#     # FishingMortality: Vector of fishing mortalities to assess
+#     # Yield: Vector of yields across FishingMortalities 
+#   
+#   # Returns: MSY value where derivative of yield is zero
+#   
+#   h_interval <- FishingMortality[2] - FishingMortality[1]
+#   #for(i in 1:length(FishingMortality)-1){
+#     YieldDeriv <- (Yield[i+1] - Yield[i])/(2*h_interval)
+#   #}
+# }
 
 
 
@@ -111,21 +279,3 @@ for(ifish in 1:length(FishingMortality)){
 # }
 
 
-
-
-
-
-
-
-
-
-
-
-# par(mfrow=c(2,2)) # this makes a 4X4 plot
-
-# !!!!!!!!!! SSB shouldn't be negative (probably because recruitment is negative)
-# !!!!!!!!!! figure out how to plot replacement line
-plot(x = SSB, y = Recruit, main = "Recruitment vs. SSB")
-plot(x = SSB, y = Yield, main = "Yield vs. SSB")
-plot(x = FishingMortality, y = Yield, main = "Yield vs. Fishing Mortality")
-plot(x = FishingMortality, y = SSB, main = "SSB vs. Fishing Mortality")
--- Hart_HW3/Hart_HW3_Functions.R
+++ Hart_HW3/Hart_HW3_Functions.R
@@ -6,22 +6,24 @@
 # Calculate Z_mortality (total mortality at age) #!!!!!! need to calculate by sex
 CalcZMortality <- function(FishingMortality = 0,
                            M_par = NULL,
-                           SelectivityAtAge = NULL){
+                           SelectivityAtAge = NULL,
+                           Ages = NULL){
   # Args
     # FishingMortality: Number between 0 and 1, corresponds to fishing mortality
     # M_par: Number between 0 and 1, represents natural mortality, no default
     # SelectivityAtAge: Matrix of proportions corresponding to selectivity at age and sex (columnames = "Selectivity_F" and "Selectivity_M"), no default
+    # Ages: Vector of fish ages
   
   # Returns: Matrix containing columns for female and male total mortality (Z) at age
   
-  ZtempList <- NULL
   Z_mortality <- matrix(NA, length(Ages),2)
   colnames(Z_mortality) <- c("Z_M", "Z_F")
   sexes <- c("F", "M")
   
   for(isex in sexes){
     for(iage in 1:length(Ages)){
-      Z_mortality[iage,paste("Z",isex, sep="_")] <- M_par + SelectivityAtAge[iage,paste("Selectivity",isex, sep="_")]*FishingMortality
+      # print(SelectivityAtAge[iage,paste("Selectivity",isex, sep="_")])
+      Z_mortality[iage,paste("Z",isex, sep="_")] <- as.numeric(M_par + SelectivityAtAge[iage,paste("Selectivity",isex, sep="_")]*FishingMortality)
     }
   }
   
@@ -30,9 +32,11 @@ CalcZMortality <- function(FishingMortality = 0,
 
 
 # Calculate NAA
-CalcNAA <- function(Z_mortality = NULL,
+CalcNAA <- function(FishingMortality = 0,
+                    Z_mortality = NULL,
                     Ages = NULL){
   # Args:
+    # FishingMortality: Number between 0 and 1, corresponds to fishing mortality
     # Z_mortality: Matrix containing columns for female and male total mortality (Z) at age, no default
     # Ages: Vector of ages
   
@@ -47,14 +51,15 @@ CalcNAA <- function(Z_mortality = NULL,
       # NAA for age 0 (first age class)
       if(iage == 1){
         NAA[iage,paste("NAA",isex, sep="_")] <- 0.5
-      } else if(iage == max(Ages)){ # NAA for last age
-        NAA[iage,paste("NAA",isex, sep="_")] <- NAA[max(Ages)-1,paste("NAA",isex, sep="_")]*exp(-Z_mortality[iage-1,paste("Z",isex, sep="_")])
-      } else{
+      } else if(iage == length(Ages)){ # NAA for last age
+        NAA[iage,paste("NAA",isex, sep="_")] <- NAA[iage-1,paste("NAA",isex, sep="_")]*exp(-Z_mortality[iage-1,paste("Z",isex, sep="_")])/(1-exp(-Z_mortality[iage,paste("Z",isex, sep="_")]))
+      } else if(1 < iage & iage < length(Ages)){
         # NAA for intermediate ages
         NAA[iage,paste("NAA",isex, sep="_")] <- NAA[iage-1,paste("NAA",isex, sep="_")]*exp(-Z_mortality[iage-1,paste("Z",isex, sep="_")])
       }
     }
   }
+  print(paste("NAA calculated at Fishing Mortality =", FishingMortality, sep=" "))
   return(NAA)
 }
 
@@ -83,6 +88,7 @@ CalcYPR <- function(FishingMortality = 0,
     for(iage in 1:length(Ages)){
       tempYPR <- WeightAtAge[iage,paste("Weight",isex, sep="_")]*SelectivityAtAge[iage,paste("Selectivity",isex, sep="_")]*FishingMortality*NAA[iage,paste("NAA",isex, sep="_")]*(1-exp(-Z_mortality[iage,paste("Z",isex, sep="_")]))/Z_mortality[iage,paste("Z",isex, sep="_")]
       tempYPRList <- c(tempYPRList, tempYPR)
+      #print(tempYPRList)
     }
   }
   #print(tempYPRList)
@@ -93,10 +99,12 @@ CalcYPR <- function(FishingMortality = 0,
 
 
 # Calculate SBPR
-CalcSBPR <- function(NAA = NULL,
+CalcSBPR <- function(FishingMortality = 0,
+                     NAA = NULL,
                      FecundityAtAge = NULL,
                      Ages = NULL){
   # Args:
+    # FishingMortality: Number between 0 and 1, corresponds to fishing mortality
     # NAA: Matrix of abundance at age by sex given FishingMortality, no default
     # FecundityAtAge: Vector of numbers corresponding to fecundity at age, no default
     # Ages: Vector of ages
@@ -110,6 +118,7 @@ CalcSBPR <- function(NAA = NULL,
   }
   SBPR <- sum(SBPRList)
   
+  print(paste("SBPR calculated at Fishing Mortality =", FishingMortality, sep=" "))
   return(SBPR)
 }
 
@@ -120,7 +129,8 @@ CalcRecruit <- function(FishingMortality = 0,
                         SBPR = NULL,
                         SBPRzero = NULL,
                         Steepness = NULL,
-                        Rzero = NULL){
+                        Rzero = NULL,
+                        gamma_par = 1){
   # Args
     # FishingMortality: Number corresponding to fishing mortalities, default = 0
     # StockRecruitForm: String specifying form of stock recruit relationship, default = "BevertonHolt"
@@ -131,40 +141,142 @@ CalcRecruit <- function(FishingMortality = 0,
     # SBPRzero: Spawnerbiomass per recruit when fishing mortality = 0, default = 0
     # Steepness: Number for steepness, no default
     # Rzero: Number, recruitment at zero fishing mortality, no default
+    # gamma_par: Number, parameter for Pella-Tomlinson stock recruit form, default = 1
   
   # Return: Number, recruitment calculated based on the StockRecruitForm
   
   if(StockRecruitForm == "BevertonHolt"){
-    Recruit <- Rzero*(0.8*SBPR*Steepness - SBPRzero*(0.2 - 0.2*Steepness))/((Steepness-0.2)*SBPR)
+    # Recruit <- Rzero*(0.8*SBPR*Steepness - SBPRzero*(0.2 - 0.2*Steepness))/((Steepness-0.2)*SBPR)
+    alpha <- SBPRzero*(0.2-0.2*Steepness)/(0.8*Steepness)
+    beta <- (0.2 - Steepness)/(-0.8*Steepness*Rzero)
+    Recruit <- (SBPR - alpha)/(beta*SBPR)
+    print(paste("Doesn't require gamma_par parameter:", gamma_par, sep=" "))
   } else if(StockRecruitForm == "Ricker"){
-    Recruit <- 0.8*SBPRzero*Rzero*(log(SBPR) - log(SBPRzero) + log(Steepness/0.2)/0.8)/(log(Steepness/0.2)*SBPR)
+    # Recruit <- 0.8*SBPRzero*Rzero*(log(SBPR) - log(SBPRzero) + log(Steepness/0.2)/0.8)/(log(Steepness/0.2)*SBPR)
+    alpha <- 1/(SBPRzero*exp(-(log(Steepness/0.2)/0.8)))
+    beta <- log(Steepness/0.2)/(0.8*SBPRzero*Rzero)
+    Recruit <- (log(alpha) + log(SBPR*Recruit))/(beta*SBPR)
+    print(paste("Doesn't require gamma_par parameter:", gamma_par, sep=" "))
   } else if(StockRecruitForm == "PellaTomlinson"){
-    Recruit <- (Rzero*SBPRzero/SBPR)*((1 + (0.2 - 0.2^(gamma+1))/(Steepness-0.2) - SBPRzero*(0.2 - 0.2^(gamma+1))/(SBPR*(Steepness - 0.2)))^(1/gamma))
+    # Recruit <- (Rzero*SBPRzero/SBPR)*((1 + (0.2 - 0.2^(gamma_par+1))/(Steepness-0.2) - SBPRzero*(0.2 - 0.2^(gamma_par+1))/(SBPR*(Steepness - 0.2)))^(1/gamma_par))
+    alpha <- 1/SBPRzero
+    beta <- (h-0.2)/(0.2 - 0.2^(gamma_par + 1))
+    Recruit <- (Rzero*SBPRzero/SBPR)*((1 + 1/beta - 1/(alpha*beta*SBPR))^(1/gamma_par))
   }
   
   return(Recruit)
 }
 
+# Calculate Rzero and SBPRzero
+CalcSBPRzero <- function(M_par = NULL,
+                         SelectivityAtAge = NULL,
+                         WeightAtAge = NULL,
+                         FecundityAtAge = NULL,
+                         gamma_par = 1,
+                         Ages = NULL,
+                         Steepness = NULL,
+                         Rzero = NULL,
+                         StockRecruitForm = "BevertonHolt"){
+  # Args: 
+  # FishingMortality: Number corresponding to fishing mortalities, default = 0
+  # M_par: Number between 0 and 1, represents natural mortality, no default
+  # SelectivityAtAge: Matrix of proportions corresponding to selectivity at age and sex (columnames = "Selectivity_F" and "Selectivity_M"), no default
+  # WeightAtAge: Vector of numbers corresponding to weight at age, no default
+  # FecundityAtAge: Vector of numbers corresponding to fecundity at age, no default
+  # gamma_par: Number, parameter for Pella-Tomlinson stock recruit relationship, default = 1
+  # Ages: Vector of ages
+  # Steepness: Number for steepness, no default
+  # Rzero: Number, recruitment at zero fishing mortality, no default
+  # StockRecruitForm: String specifying form of stock recruit relationship, default = "BevertonHolt"
+  # = "BevertonHolt"
+  # = "Ricker"
+  # = "PellaTomlinson
+  
+  # Return: SBPRzero
+  
+  FishingMortality <- 0
+  Z_mortality <- CalcZMortality(FishingMortality = 0, M_par = M_par, SelectivityAtAge = SelectivityAtAge, Ages = Ages)
+  NAA <- CalcNAA(Z_mortality = Z_mortality, Ages = Ages)
+  SBPRzero <- CalcSBPR(NAA = NAA, FecundityAtAge = FecundityAtAge, Ages = Ages)
+  
+  return(SBPRzero)
+}
+
 
 # Calculate Yield
-CalcYield <- function(YPR = NULL,
-                      Recruit = NULL){
+CalcYield <- function(FishingMortality = 0,
+                      M_par = NULL,
+                      SelectivityAtAge = NULL,
+                      WeightAtAge = NULL,
+                      FecundityAtAge = NULL,
+                      gamma_par = 1,
+                      Ages = NULL,
+                      Steepness = NULL,
+                      Rzero = NULL,
+                      StockRecruitForm = "BevertonHolt"){
   # Args: 
-    # Recruit: Number corresponding to recruitment, no default
-    # YPR: Number corresponding to Yield Per Recruit, no default
+    # FishingMortality: Number corresponding to fishing mortalities, default = 0
+    # M_par: Number between 0 and 1, represents natural mortality, no default
+    # SelectivityAtAge: Matrix of proportions corresponding to selectivity at age and sex (columnames = "Selectivity_F" and "Selectivity_M"), no default
+    # WeightAtAge: Vector of numbers corresponding to weight at age, no default
+    # FecundityAtAge: Vector of numbers corresponding to fecundity at age, no default
+    # gamma_par: Number, parameter for Pella-Tomlinson stock recruit relationship, default = 1
+    # Ages: Vector of ages
+    # Steepness: Number for steepness, no default
+    # Rzero: Number, recruitment at zero fishing mortality, no default
+    # StockRecruitForm: String specifying form of stock recruit relationship, default = "BevertonHolt"
+      # = "BevertonHolt"
+      # = "Ricker"
+      # = "PellaTomlinson
+    
+  # Return: List containing calculated Yield with associated FishingMortality, Recruitment, SBPR, NAA, YPR, SBPRzero, and Z_mortality
   
-  # Return: Number corresponding to Yield
+  Ages <- seq(0,length(FecundityAtAge)-1)
   
-  Yield <- YPR*Recruits
+  # First calculate SBPRzero
+  SBPRzero <- CalcSBPRzero(M_par = M_par, SelectivityAtAge = SelectivityAtAge, WeightAtAge = WeightAtAge, FecundityAtAge = FecundityAtAge, 
+                           gamma_par = gamma_par, Ages = Ages, Steepness = Steepness, Rzero = Rzero, StockRecruitForm = StockRecruitForm)
+  print(SBPRzero)
+  # Calculate total mortality at age for both sexes
+  Z_mortality <- CalcZMortality(FishingMortality = FishingMortality, M_par = M_par, SelectivityAtAge = SelectivityAtAge, Ages = Ages)
+  # Calculate abundance at age for both sexes
+  NAA <- CalcNAA(FishingMortality = FishingMortality, Z_mortality = Z_mortality, Ages = Ages)
+  # Calculate Yield Per Recruit
+  YPR <- CalcYPR(FishingMortality = FishingMortality, WeightAtAge = WeightAtAge, SelectivityAtAge = SelectivityAtAge, NAA = NAA, Z_mortality = Z_mortality, Ages = Ages)
+  # Calculate Spawner Biomas Per Recruit
+  SBPR <- CalcSBPR(FishingMortality = FishingMortality, NAA = NAA, FecundityAtAge = FecundityAtAge, Ages = Ages)
+  # Calculate recruitment
+  # print(FishingMortality)
+  # print(SBPR)
+  # print(SBPRzero)
+  # print(Rzero)
+  # print(Steepness)
+  Recruit <- CalcRecruit(FishingMortality = FishingMortality, StockRecruitForm = StockRecruitForm, SBPR = SBPR, SBPRzero = SBPRzero, Steepness = Steepness, Rzero = Rzero, gamma_par=gamma_par)
+  # Calculate yield
+  Yield <- YPR*Recruit
   
-  return(Yield)
+  Result <- NULL
+  Result$Yield <- Yield
+  Result$Recruit <- Recruit
+  # print(Recruit)
+  # print(SBPR)
+  Result$FishingMortality <- FishingMortality
+  Result$SBPR <- SBPR
+  Result$NAA <- NAA
+  Result$Z_mortality <- Z_mortality
+  Result$YPR <- YPR
+  Result$SBPRzero <- SBPRzero
+  
+  return(Result)
 }
 
 
 # Calculate SSB
-CalcSSB <- function(SBPR = NULL,
+CalcSSB <- function(FishingMortality = 0,
+                    SBPR = NULL,
                     Recruit = NULL){
   # Args:
+    # FishingMortality: Number between 0 and 1, corresponds to fishing mortality
     # SBPR: Number corresponding to spawner biomass per recruit, no default
     # Recruit: Number corresponding to recruitment, no default
     
@@ -172,6 +284,12 @@ CalcSSB <- function(SBPR = NULL,
   
   SSB <- SBPR*Recruit
   
+  print(paste("SSB calculated at Fishing Mortality =", FishingMortality, sep=" "))
   return(SSB)
 }
+
+
+
+
+
 
